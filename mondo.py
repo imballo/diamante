c0298c9b3ff1deaa176f8a676269247c57a30ba2
imballo <37402656+imballo@users.noreply.github.com>
2018-04-30 19:02:05 +0200

    Add files via upload

--- mondo.py
+++ mondo.py
@@ -14,7 +14,7 @@ class CasellaInizio(CasellaMappa):
         if self.visitata:
             return "\nQuesta è una zona noiosa della caverna.\n"
         else:
-            s=open('./immagini_storie/immagine_inizio.txt').read()
+            s=open('./immagini_storie/immagine_inizio.txt', encoding='utf8').read()
             s=s+"""
 Ti trovi in una caverna alla ricerca di una pietra leggendaria.
 Trovarla o meno sarà solo merito tuo.
@@ -39,7 +39,7 @@ class CasellaFine(CasellaMappa):
         return 'FF'
     
     def descrizione(self):
-        d=open('./immagini_storie/immagine_fine.txt').read()        
+        d=open('./immagini_storie/immagine_fine.txt', encoding='utf8').read()        
         d1= \
 """
 Vedi il luccichio di una pietra in lontananza.
@@ -196,7 +196,7 @@ class CasellaBoss(CasellaMostro):
     def descrizione(self, veloce=False):
         s=''
         if self.nemico.vivo() and not self.visitata:
-            s=open('./immagini_storie/immagine_boss.txt').read()
+            s=open('./immagini_storie/immagine_boss.txt', encoding='utf8').read()
         return s+super().descrizione()
             
 class CasellaCommerciante(CasellaMappa):
@@ -361,7 +361,7 @@ Mappa:
                 if cella == casella_giocatore:
                     mappa +='##' #print('##',end='')
                 elif cella.visitata or self.facile:
-                    mappa +=str(cella) # print(cella,end='')
+                    mappa += str(cella) # print(cella,end='')
                 else:
                     mappa +='**' #print('**',end='')
             mappa+='║'
